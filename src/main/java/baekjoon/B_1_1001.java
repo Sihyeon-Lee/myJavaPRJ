545b79294f2f5619b5f4d90c59d6400a644488e1
Leesihyeon <pubu0880@naver.com>
2023-04-04 19:05:57 +0900

    2023-04-04 백준 1000번 문제

---
+++ src/main/java/baekjoon/B_1_1001.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_1_1001 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a, b;
+
+        a = s.nextInt();
+        b = s.nextInt();
+
+        System.out.printf("%d", a-b);
+    }
+}

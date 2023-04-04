19752e87200184aacbccf1077e0a97e0f37c359f
Leesihyeon <pubu0880@naver.com>
2023-04-04 16:53:46 +0900

    2023-04-04 백준 1330번 문제

---
+++ src/main/java/Baekjoon1330.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Baekjoon1330 {
+    public static void main (String[] args) {
+
+        int a, b;
+
+        Scanner s = new Scanner(System.in);
+
+        a = s.nextInt();
+        b = s.nextInt();
+
+        if ( a > b ) {
+            System.out.println(">");
+        } else if ( a < b ) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}

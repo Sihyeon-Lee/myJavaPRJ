49688993a75c9a01e64e46a1ac281c7f8047607a
Leesihyeon <pubu0880@naver.com>
2023-04-04 19:06:29 +0900

    2023-04-04 백준 10998번 문제

---
+++ src/main/java/baekjoon/B_1_10998.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_1_10998 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.print(a*b);
+
+
+    }
+}

850f8467c192de7b23478e45ab49b1b5292fbb40
Leesihyeon <pubu0880@naver.com>
2023-05-03 10:11:01 +0900

    교재실습 9-2

---
+++ src/main/java/Ex/Ex09_02.java
@@ -0,0 +1,24 @@
+package Ex;
+
+import java.util.Scanner;
+
+public class Ex09_02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str;
+
+        System.out.print("문자열 입력 ==> ");
+        str = s.nextLine();
+
+        System.out.print("출력 문자열 ==> ");
+        for  (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'o')
+                System.out.printf("%c", '$');
+            else
+                System.out.printf("%c", str.charAt(i));
+        }
+        System.out.println();
+        System.out.println("내가 사용한 함수 : " + str.replaceAll("o", "q"));
+//        for문을 replaceAll 함수를 써서 대신할수 있음
+    }
+}

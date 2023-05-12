e52ba1705549cc8165e0f74c4e1e2f2f039e44bd
Leesihyeon <pubu0880@naver.com>
2023-05-12 15:01:32 +0900

    2023-05-12 백준 27866번 문제

---
+++ src/main/java/baekjoon/B_5_27866.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_5_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int i = sc.nextInt();
+
+        System.out.print(s.charAt(i-1));
+    }
+}

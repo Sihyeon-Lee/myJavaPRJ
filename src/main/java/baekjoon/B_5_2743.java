e7189e2d8aadc29264f2a64d59dae4e81dab8aff
leeshhyeon <pubu0880@naver.com>
2023-06-30 19:07:02 +0900

    2023-06-30 백준 2743문제

---
+++ src/main/java/baekjoon/B_5_2743.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_5_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str;
+
+        str = sc.next();
+
+        System.out.print(str.length());
+
+    }
+}

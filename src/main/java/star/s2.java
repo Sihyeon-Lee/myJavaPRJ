372b6e896b99df3292dcf0a046641b405cea37f9
leesihyeon <pubu0880@naver.com>
2023-08-24 09:41:40 +0900

    별찍기 재연습

---
+++ src/main/java/star/s2.java
@@ -0,0 +1,21 @@
+package star;
+
+import java.util.Scanner;
+
+public class s2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
